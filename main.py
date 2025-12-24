fc6cee9b3126c473c09a0221870edc587356e159
NARUTOisthebest12 <stavya.adlakha@gmail.com>
2025-12-24 07:18:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-with open("Codingal.txt","w") as file:
-    file.write("Hi I am Naruto and I am 1 yr old.")
-    file.close()
+new_file = open('New_File.txt','x')
+new_file.close()
 
-with open("Codingal.txt","r") as file:
-    data = file.readlines()
-    print("Words in this file are......")
-    for line in data:
-        word = line.split()
-        print(word)
-        file.close()
+import os
+print("Cheking if my file exists or not....")
+if os.path.exists("my_file.txt"):
+    os.remove("my_file.txt")
+else: 
+    print("The file does not exist")
+
+my_file = open('my_file.txt','w')
+my_file.write("Hi i am naruto and i am 1 yr old")    
+my_file.close()
+
+os.remove('Codingal.txt')
+
+os.rmdir('Folder')
